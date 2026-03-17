2e484c0a4febfced8d184d85559d3d16cd945044
ChandraMouli2727 <155448204+ChandraMouli2727@users.noreply.github.com>
2026-03-17 13:30:36 +0530

    Create 2_Variable.py

---
+++ Python/Exercise/2_Variable.py
@@ -0,0 +1,76 @@
+
+'''
+break=5
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 1
+    break=5
+         ^
+SyntaxError: invalid syntax '''
+
+birth_year=2001
+current_year=2026
+age=current_year-birth_year
+print(age)
+
+first="Perumallagari "
+middle="Chandra "
+last="Mouli"
+print("My full name is: " + first + " " + middle + " " + last)
+
+
+'''
+1record = a
+print(1record)
+
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 1
+    1record = a
+    ^
+SyntaxError: invalid decimal literal
+'''
+
+
+
+'''
+record-one = b
+print(record-one)
+
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 2
+    record-one = b
+    ^^^^^^^^^^
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+'''
+
+
+
+'''
+record^one = c
+print(record^one)
+
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 4
+    record^one = c
+    ^^^^^^^^^^
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+'''
+
+
+
+'''
+
+continue = d
+print(continue)
+
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 2
+    continue = d
+             ^
+SyntaxError: invalid syntax
+'''
+
