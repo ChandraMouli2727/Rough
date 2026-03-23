68b0e97c5850157f3cbd64d8e502521ddf488c1f
ChandraMouli2727 <155448204+ChandraMouli2727@users.noreply.github.com>
2026-03-23 17:43:19 +0530

    Update Hash_Table_Collosion.py

--- DSA/Hash_Table_Collosion.py
+++ DSA/Hash_Table_Collosion.py
@@ -40,9 +40,10 @@ print(a)
 
 class HashTable:
     def __init__(self):
-        self.Max = 100
-        self.arr = ['' for i in range(self.Max)]
-        
+        self.Max = 10
+        # self.arr = ['' for i in range(self.Max)]
+        self.arr = [[] for i in range(self.Max)]  # collision handling
+ 
     def get_hash(self,key):
         h = 0
         for char in key:
@@ -59,15 +60,29 @@ class HashTable:
     
     def __setitem__(self,key,value):
         h = self.get_hash(key)
-        self.arr[h] = value
+        # self.arr[h] = value
+        found = False
+        for idx,element in enumerate (self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+               self.arr[h][idx] = (key,value) # tuple is immutable so directly removed a element and added
+               found = True
+               break
+        if not found:
+           self.arr[h].append((key,value))
     
     def __getitem__(self,key):
         h = self.get_hash(key) 
-        return self.arr[h]
+        #return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
     
     def __delitem__(self,key):
         h = self.get_hash(key)
-        self.arr[h] = None
+        # self.arr[h] = None
+        for index,element in enumerate (self.arr[h]):
+            if element[0] == key:
+                del self.arr[h][index]
         
 
 t = HashTable()
@@ -86,3 +101,19 @@ print(f)
 del t['march 5']
 g = t.arr
 print(g)
+
+'''
+chaining / collosion means hash code is same for 2 keys we can store it as key value 2.method  is linear probing it will search empty slot and store it
+'''
+tt = HashTable()
+z = tt.get_hash("march 6")
+print(z)
+y = tt.get_hash("march 17")
+print(y)
+tt['march 6'] = 420
+tt['march 17'] = 520
+tt['a@XYZ'] = 320
+print(tt.arr)
+del tt['a@XYZ']
+print(tt.arr)
+print(tt['march 17'])
