6c29fd0280c9c8dd166037a86990058e2c973d82
Chandra Mouli <chandramouli2727@gmail.com>
2026-03-24 07:30:11 +0530

    updated Hash Table Ex file

--- DSA/Hash_Table_ex.py
+++ DSA/Hash_Table_ex.py
@@ -74,3 +74,81 @@ with open("nyc_weather.csv","r") as f:
 
 
 
+arr = []
+with open("C:\\Users\\Chandra Mouli\\Desktop\\Ai_Eng\\rough\\nyc_weather.csv","r") as f :
+    for line in f:
+         tokens = line.split(',')
+         try:
+             temperature = int(tokens[1])
+             arr.append(temperature)
+         except:
+             print("Invalid temperature.Ignore the row")
+
+print(arr)
+'''
+Exercise: Hash Table: New York City Weather Analysis
+(1) nyc_weather.csv contains new york city weather for first few days in the month of January. Write a program that can answer following,
+
+  (a) What was the average temperature in first week of Jan
+
+  (b) What was the maximum temperature in first 10 days of Jan
+
+  Figure out data structure that is best for this problem
+'''
+
+avg = sum(arr[0:7])/len(arr[0:7])
+print(avg)
+
+max_m = max(arr[0:10])
+
+print(max_m)
+
+''' The best data structure to use here was a list because all we wanted was access of temperature elements '''
+
+'''
+(2) nyc_weather.csv contains new york city weather for first few days in the month of January. Write a program that can answer following,
+
+  (a) What was the temperature on Jan 9?
+
+  (b) What was the temperature on Jan 4?
+
+  Figure out data structure that is best for this problem
+  
+  
+The best data structure to use here was a dictionary (internally a hash table) because we wanted to know temperature for specific day, 
+requiring key, value pair access where you can look up an element by day using O(1) complexity
+'''
+
+weather_dict = {}
+
+with open("C:\\Users\\Chandra Mouli\\Desktop\\Ai_Eng\\rough\\nyc_weather.csv","r") as f:
+    for line in f:
+        tokens = line.split(',')
+        day = tokens[0]
+        try:
+            temperature = int(tokens[1])
+            weather_dict[day] = temperature
+        except:
+            print("Invalid temperature.Ignore the row")
+
+print(weather_dict)
+print(weather_dict['Jan 9'])
+print(weather_dict['Jan 4'])
+
+with open("C:\\Users\\Chandra Mouli\\Desktop\\Ai_Eng\\rough\\poem.txt","r") as f:
+    for line in f:
+        print(line)
+
+
+word_count = {}
+with open("C:\\Users\\Chandra Mouli\\Desktop\\Ai_Eng\\rough\\poem.txt","r") as f:
+    for line in f:
+        tokens = line.split(' ')
+        for token in tokens:
+            token=token.replace('\n','')
+            if token in word_count:
+                word_count[token]+=1
+            else:
+                word_count[token]=1
+
+print(word_count)
